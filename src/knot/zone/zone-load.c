CZ-NIC/knot

redis: binary alternatives

src/knot/zone/zone-load.c
@@ -31,27 +31,42 @@ int zone_load_contents(conf_t *conf, const knot_dname_t *zone_name,
 	if (conf == NULL || zone_name == NULL || contents == NULL) {
 		return KNOT_EINVAL;
 	}
+	conf_val_t val = conf_zone_get(conf, C_ZONE_BACKEND, zone_name);
+	unsigned backend = conf_opt(&val);
 
-	char *zonefile = conf_zonefile(conf, zone_name);
-	conf_val_t val = conf_zone_get(conf, C_DEFAULT_TTL, zone_name);
-	uint32_t dflt_ttl = conf_int(&val);
-
-	zloader_t zl;
-	int ret = zonefile_open(&zl, zonefile, zone_name, dflt_ttl,
-	                        semcheck_mode, time(NULL));
-	free(zonefile);
-	if (ret != KNOT_EOK) {
-		return ret;
-	}
-
+	zloader_t loader;
 	sem_handler_t handler = {
 		.cb = err_handler_logger
 	};
 
-	zl.err_handler = &handler;
+	int ret;
+	if (backend == ZONE_BACKEND_FILE) {
+		char *zonefile = conf_zonefile(conf, zone_name);
+		val = conf_zone_get(conf, C_DEFAULT_TTL, zone_name);
+		uint32_t dflt_ttl = conf_int(&val);
 
-	*contents = zonefile_load(&zl);
-	zonefile_close(&zl);
+		ret = zonefile_open(&loader, zonefile, zone_name, dflt_ttl,
+		                    semcheck_mode, &handler, time(NULL));
+		free(zonefile);
+	} else {
+		conf_val_t db_listen = conf_db_param(conf, C_ZONE_DB_LISTEN);
+		struct sockaddr_storage addr = conf_addr(&db_listen, NULL);
+		int port = sockaddr_port(&addr);
+		sockaddr_port_set(&addr, 0);
+		char addr_str[SOCKADDR_STRLEN];
+		if (port <= 0 || sockaddr_tostr(addr_str, sizeof(addr_str), &addr) <= 0) {
+			return KNOT_EINVAL;
+		}
+
+		ret = zone_rdb_open(&loader, zone_name, addr_str, port,
+		                    semcheck_mode, &handler, time(NULL));
+	}
+	if (ret != KNOT_EOK) {
+		return ret;
+	}
+
+	*contents = zonefile_load(&loader);
+	zonefile_close(&loader);
 	if (*contents == NULL) {
 		return KNOT_ERROR;
 	}

offsoc/knot-dns

src/knot/zone/zone-load.c
@@ -21,6 +21,9 @@ int zone_load_contents(conf_t *conf, const knot_dname_t *zone_name,
 		return KNOT_EINVAL;
 	}
 
+	conf_val_t val = conf_zone_get(conf, C_ZONE_BACKEND, zone_name);
+	unsigned backend = conf_opt(&val);
+
 	zone_skip_t skip = { 0 };
 	conf_val_t conf_skip = conf_zone_get(conf, C_ZONEFILE_SKIP, zone_name);
 	int ret = zone_skip_from_conf(&skip, &conf_skip);
@@ -28,28 +31,40 @@ int zone_load_contents(conf_t *conf, const knot_dname_t *zone_name,
 		return ret;
 	}
 
-	char *zonefile = conf_zonefile(conf, zone_name);
-	conf_val_t val = conf_zone_get(conf, C_DEFAULT_TTL, zone_name);
-	uint32_t dflt_ttl = conf_int(&val);
+	zloader_t loader;
+	sem_handler_t handler = {
+		.cb = err_handler_logger
+	};
 
-	zloader_t zl;
-	ret = zonefile_open(&zl, zonefile, zone_name, dflt_ttl,
-	                    semcheck_mode, time(NULL));
-	free(zonefile);
+	if (backend == ZONE_BACKEND_FILE) {
+		char *zonefile = conf_zonefile(conf, zone_name);
+		val = conf_zone_get(conf, C_DEFAULT_TTL, zone_name);
+		uint32_t dflt_ttl = conf_int(&val);
+
+		ret = zonefile_open(&loader, zonefile, zone_name, dflt_ttl,
+		                    semcheck_mode, &handler, time(NULL), &skip);
+		free(zonefile);
+	} else {
+#ifdef ENABLE_REDIS
+		redisContext *rdb = zone_rdb_connect(conf);
+		if (rdb == NULL) {
+			zone_skip_free(&skip);
+			return KNOT_ECONN;
+		}
+
+		ret = zone_rdb_open(&loader, rdb, zone_name, semcheck_mode,
+		                    &handler, time(NULL), &skip);
+#else
+		ret = KNOT_ENOTSUP;
+#endif
+	}
 	if (ret != KNOT_EOK) {
 		zone_skip_free(&skip);
 		return ret;
 	}
 
-	sem_handler_t handler = {
-		.cb = err_handler_logger
-	};
-
-	zl.err_handler = &handler;
-	zl.creator->skip = &skip;
-
-	*contents = zonefile_load(&zl, 0);
-	zonefile_close(&zl);
+	*contents = zonefile_load(&loader, 0);
+	zonefile_close(&loader);
 	zone_skip_free(&skip);
 	if (*contents == NULL) {
 		return KNOT_ERROR;